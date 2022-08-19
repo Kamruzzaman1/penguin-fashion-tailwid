7c76ad3b3a2d112823e35cda197efc4831fd9aba
kamruzzaman1 <kamruzzamansazal1@gmail.com>
2022-08-19 07:42:27 +0600

    add js

---
+++ js/penguin.js
@@ -0,0 +1,4 @@
+document.getElementById('buyNow-btn').addEventListener('click',function(){
+    const buttonBuyNow = document.getElementById('text-change');
+    buttonBuyNow.innerText = 'i am the penguin!!'
+})
